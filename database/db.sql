11e55ce3ab861b2a9cd01ece7b3b62c9072ed4b6
AndikaPermanaSidiq01 <andikapermanasidiq00@gmail.com>
2020-11-28 18:32:58 +0700

    Update the database

--- database/db.sql
+++ database/db.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v11.11 (64 bit)
 MySQL - 5.5.5-10.4.13-MariaDB : Database - btis
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -16,22 +17,61 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`btis` /*!40100 DEFAULT CHARACTER SET ut
 
 USE `btis`;
 
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`role`) values (1,'Admin'),(2,'Member');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id_user` char(8) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `nomer_telp` varchar(20) DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `foto_profil` varchar(100) DEFAULT 'default.jpg',
+  `role_id` int(11) DEFAULT NULL,
+  `is_active` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id_user`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id_user`,`username`,`email`,`password`,`nomer_telp`,`tanggal_lahir`,`foto_profil`,`role_id`,`is_active`) values ('5fa7963b','Andika Permana Sidiq','andika@gmail.com','$2y$10$4DipOGtUYRFbZQ953q6qSuQP812oYCu8v6nYhnrzVvzNZrOtf/Jau','12345','2020-11-04','user1606216321.jpg',1,1),('user9046','Satou Kazuma','kazuma@gmail.com','$2y$10$7mBRJP52sI.oYrlZ4m76peN/ys2EhUGIHbqAu6J4/IW8ul515wE5O','085321874357','1994-01-26','default.jpg',2,1);
+
 /*Table structure for table `alamat` */
 
 DROP TABLE IF EXISTS `alamat`;
 
 CREATE TABLE `alamat` (
-  `id_alamat` char(5) NOT NULL,
+  `id_alamat` int(11) NOT NULL AUTO_INCREMENT,
   `alamat` varchar(255) DEFAULT NULL,
   `penerima` varchar(100) DEFAULT NULL,
-  `nomer_telp_penerima` varchar(50) DEFAULT NULL,
+  `telepon_penerima` varchar(50) DEFAULT NULL,
   `id_user` char(8) DEFAULT NULL,
   PRIMARY KEY (`id_alamat`),
   KEY `id_user` (`id_user`),
-  CONSTRAINT `alamat_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `alamat_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `alamat` */
+
+insert  into `alamat`(`id_alamat`,`alamat`,`penerima`,`telepon_penerima`,`id_user`) values (1,'Kp. Walahar 1 RT 009/003, Desa Bantarwaru, Kec. Gantar, Kab. Indramayu','Andika Permana Sidiq','62 853 2187 4357','user9046'),(4,'Jl. Sukajaya Kaler No. 44 RT.05/04, Kel. Cibabat Kec. Cimahi Utara','Satou Kazuma','08888888888','user9046');
 
 /*Table structure for table `bank` */
 
@@ -43,9 +83,49 @@ CREATE TABLE `bank` (
   `norek` varchar(150) DEFAULT NULL,
   `atas_nama` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_bank`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `bank` */
+
+insert  into `bank`(`id_bank`,`nama_bank`,`norek`,`atas_nama`) values (1,'BRI','3388 - 01 - 028216 - 53 - 5','Pikapikani'),(3,'BNI','0856577142','Pikapikani');
+
+/*Table structure for table `kategori` */
+
+DROP TABLE IF EXISTS `kategori`;
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_kategori` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_kategori`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `kategori` */
+
+insert  into `kategori`(`id_kategori`,`nama_kategori`) values (5,'Kaos'),(6,'Tas Ransel'),(7,'Sling Bag'),(8,'Bandana'),(9,'Waist Bag'),(10,'Polo Shirt'),(11,'Cover Bag'),(12,'Emblem Bordir'),(13,'Hoodie');
+
+/*Table structure for table `produk` */
+
+DROP TABLE IF EXISTS `produk`;
+
+CREATE TABLE `produk` (
+  `id_produk` char(25) NOT NULL,
+  `nama_produk` varchar(100) DEFAULT NULL,
+  `label_produk` varchar(100) DEFAULT NULL,
+  `ukuran` varchar(100) DEFAULT NULL,
+  `stok` varchar(100) DEFAULT NULL,
+  `id_kategori` int(11) DEFAULT NULL,
+  `deskripsi_produk` text DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `foto_produk` varchar(100) DEFAULT NULL,
+  `terjual` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_produk`),
+  KEY `produk_ibfk_1` (`id_kategori`),
+  CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `produk` */
+
+insert  into `produk`(`id_produk`,`nama_produk`,`label_produk`,`ukuran`,`stok`,`id_kategori`,`deskripsi_produk`,`harga`,`foto_produk`,`terjual`) values ('5fb4afd571bfb','Hoodie Black Clover','Black Clover','L,XL','4',13,'<p>Hoodie ini bisa dipakai oleh pria ataupun wanita (unisex). Hoodie ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p>',240000,'hoodie.jpg',0),('5fb4b00573cd1','Kaos Anime','Anime','M,L,XL','4',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',98000,'anime.jpg',1),('5fb4b02aa530f','Kaos Elizabeth','Gintama','S,M,L,XL','2',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',98000,'gintama.jpg',0),('5fb4b06c8b76e','Kaos Happy','Fairy Tail','L,XL,XXL','1',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',98000,'hapi.jpg',1),('5fb4b08e41896','Kaos Gon','Hunter X Hunter','L,XL,XXL','5',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',98000,'gon.jpg',0),('5fb4b0d4bb9a8','Kaos God Usopp','One Piece','S,M,L,XL,XXL','10',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',98000,'hero.jpg',0),('5fbd2a06e0248','Kaos Onigiri','Jepang','M,L,XL,XXL','5',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',110000,'onigiri.jpg',0);
 
 /*Table structure for table `diskon` */
 
@@ -78,23 +158,11 @@ CREATE TABLE `favorit` (
   KEY `id_produk` (`id_produk`),
   CONSTRAINT `favorit_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `favorit_ibfk_3` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `favorit` */
 
-/*Table structure for table `kategori` */
-
-DROP TABLE IF EXISTS `kategori`;
-
-CREATE TABLE `kategori` (
-  `id_kategori` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_kategori` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id_kategori`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `kategori` */
-
-insert  into `kategori`(`id_kategori`,`nama_kategori`) values (5,'Kaos'),(6,'Tas Ransel'),(7,'Sling Bag'),(8,'Bandana'),(9,'Waist Bag'),(10,'Polo Shirt'),(11,'Cover Bag'),(12,'Emblem Bordir'),(13,'Hoodie');
+insert  into `favorit`(`id_favorit`,`id_produk`,`id_user`) values (1,'5fb4b08e41896','user9046'),(8,'5fb4b02aa530f','user9046'),(21,'5fb4b0d4bb9a8','user9046'),(22,'5fbd2a06e0248','user9046');
 
 /*Table structure for table `keranjang` */
 
@@ -103,13 +171,15 @@ DROP TABLE IF EXISTS `keranjang`;
 CREATE TABLE `keranjang` (
   `id_keranjang` int(11) NOT NULL AUTO_INCREMENT,
   `id_produk` char(20) DEFAULT NULL,
+  `ukuran` char(3) DEFAULT NULL,
+  `jml_beli` int(11) DEFAULT NULL,
   `id_user` char(8) DEFAULT NULL,
   PRIMARY KEY (`id_keranjang`),
   KEY `id_user` (`id_user`),
   KEY `id_produk` (`id_produk`),
   CONSTRAINT `keranjang_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `keranjang_ibfk_3` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `keranjang` */
 
@@ -140,67 +210,34 @@ insert  into `kontak`(`id`,`alamat`,`facebook`,`email`,`instagram`,`telepon`,`li
 DROP TABLE IF EXISTS `pesanan`;
 
 CREATE TABLE `pesanan` (
-  `id_pesanan` char(8) NOT NULL,
-  `id_produk` char(20) DEFAULT NULL,
-  `id_alamat` char(5) DEFAULT NULL,
+  `id_pesanan` varchar(50) NOT NULL,
+  `id_produk` char(25) DEFAULT NULL,
+  `id_alamat` int(11) DEFAULT NULL,
   `id_bank` int(11) DEFAULT NULL,
-  `total_bayar` int(11) DEFAULT NULL,
   `id_user` char(8) DEFAULT NULL,
+  `ukuran_produk` varchar(10) DEFAULT NULL,
+  `jml_beli` int(11) DEFAULT NULL,
+  `total_bayar` int(11) DEFAULT NULL,
   `kurir` varchar(100) DEFAULT 'J&T Express',
   `resi_pengiriman` varchar(50) DEFAULT NULL,
   `status_pengiriman` int(1) DEFAULT 0,
   `bukti_transfer` varchar(255) DEFAULT NULL,
+  `status_pembayaran` int(1) DEFAULT 0,
   `status_pemesanan` int(1) DEFAULT 0,
   PRIMARY KEY (`id_pesanan`),
   KEY `pesanan_ibfk_1` (`id_bank`),
-  KEY `pesanan_ibfk_3` (`id_alamat`),
   KEY `pesanan_ibfk_4` (`id_user`),
-  KEY `id_produk` (`id_produk`),
+  KEY `id_keranjang` (`id_produk`),
+  KEY `id_alamat` (`id_alamat`),
   CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`id_bank`) REFERENCES `bank` (`id_bank`),
-  CONSTRAINT `pesanan_ibfk_3` FOREIGN KEY (`id_alamat`) REFERENCES `alamat` (`id_alamat`),
   CONSTRAINT `pesanan_ibfk_4` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
-  CONSTRAINT `pesanan_ibfk_5` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`)
+  CONSTRAINT `pesanan_ibfk_7` FOREIGN KEY (`id_alamat`) REFERENCES `alamat` (`id_alamat`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `pesanan` */
 
-/*Table structure for table `produk` */
+insert  into `pesanan`(`id_pesanan`,`id_produk`,`id_alamat`,`id_bank`,`id_user`,`ukuran_produk`,`jml_beli`,`total_bayar`,`kurir`,`resi_pengiriman`,`status_pengiriman`,`bukti_transfer`,`status_pembayaran`,`status_pemesanan`) values ('ps-user9046-1606548430','5fb4b06c8b76e',1,3,'user9046','XL',1,83300,'J&T Express','99999',1,'bukti20201128142739.png',1,1);
 
-DROP TABLE IF EXISTS `produk`;
-
-CREATE TABLE `produk` (
-  `id_produk` char(25) NOT NULL,
-  `nama_produk` varchar(100) DEFAULT NULL,
-  `label_produk` varchar(100) DEFAULT NULL,
-  `ukuran` varchar(100) DEFAULT NULL,
-  `stok` varchar(100) DEFAULT NULL,
-  `id_kategori` int(11) DEFAULT NULL,
-  `deskripsi_produk` text DEFAULT NULL,
-  `harga` int(11) DEFAULT NULL,
-  `foto_produk` varchar(100) DEFAULT NULL,
-  `terjual` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_produk`),
-  KEY `produk_ibfk_1` (`id_kategori`),
-  CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `produk` */
-
-insert  into `produk`(`id_produk`,`nama_produk`,`label_produk`,`ukuran`,`stok`,`id_kategori`,`deskripsi_produk`,`harga`,`foto_produk`,`terjual`) values ('5fb3f08c6c231','Kaos Onigiri','Jepang','M,L,XL,XXL','22',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',110000,'onigiri.jpg',2),('5fb4afd571bfb','Hoodie Black Clover','Black Clover','L,XL','12',13,'<p>Hoodie ini bisa dipakai oleh pria ataupun wanita (unisex). Hoodie ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p>',240000,'hoodie.jpg',22),('5fb4b00573cd1','Kaos Anime','Anime','M,L,XL','5',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',98000,'anime.jpg',21),('5fb4b02aa530f','Kaos Elizabeth','Gintama','S,M,L,XL','5',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',98000,'gintama.jpg',55),('5fb4b06c8b76e','Kaos Happy','Fairy Tail','L,XL,XXL','5',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',98000,'hapi.jpg',54),('5fb4b08e41896','Kaos Gon','Hunter X Hunter','L,XL,XXL','23',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',98000,'gon.jpg',50),('5fb4b0d4bb9a8','Kaos God Usopp','One Piece','S,M,L,XL,XXL','10',5,'<p>Kaos ini bisa dipakai oleh pria ataupun wanita (unisex). Kaos ini terbuat dari bahan Cotton Combed 30s yang menyerap keringat, lembut, dingin dan nyaman saat dipakai. Dan menggunakan sablon Plastisol yang pastinya tidak mudah luntur dan sangat awet</p><p><br></p><p>Size Kaos :</p><p>M : 66cm x 49cm</p><p>L : 69cm x 51cm</p><p>XL : 71cm x 52cm</p><p>XXL : 74cm x 54cm</p>',98000,'hero.jpg',99);
-
-/*Table structure for table `role` */
-
-DROP TABLE IF EXISTS `role`;
-
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `role` */
-
-insert  into `role`(`id`,`role`) values (1,'Admin'),(2,'Member');
 
 /*Table structure for table `token` */
 
@@ -215,28 +252,7 @@ CREATE TABLE `token` (
 
 /*Data for the table `token` */
 
-/*Table structure for table `user` */
 
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE `user` (
-  `id_user` char(8) NOT NULL,
-  `username` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `nomer_telp` varchar(20) DEFAULT NULL,
-  `tanggal_lahir` date DEFAULT NULL,
-  `foto_profil` varchar(100) DEFAULT 'default.jpg',
-  `role_id` int(11) DEFAULT NULL,
-  `is_active` int(1) DEFAULT NULL,
-  PRIMARY KEY (`id_user`),
-  KEY `role_id` (`role_id`),
-  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `user` */
-
-insert  into `user`(`id_user`,`username`,`email`,`password`,`nomer_telp`,`tanggal_lahir`,`foto_profil`,`role_id`,`is_active`) values ('5fa7963b','Andika Permana Sidiq','andika@gmail.com','$2y$10$4DipOGtUYRFbZQ953q6qSuQP812oYCu8v6nYhnrzVvzNZrOtf/Jau','','0000-00-00','liz.jpg',1,1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
